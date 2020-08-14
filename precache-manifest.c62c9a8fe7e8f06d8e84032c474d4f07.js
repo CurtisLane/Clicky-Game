82a47d331c12f93be7f99cbe640cce7670be33e5
Curtis Lane <clanelmt@gmail.com>
2020-08-14 19:26:22 -0400

    Updates

---
+++ precache-manifest.c62c9a8fe7e8f06d8e84032c474d4f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1daf25d2de6cb5185a77780b8757e8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "432bf326c857ad900889",
+    "url": "/Clicky-Game/static/css/main.2e954e72.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/Clicky-Game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432bf326c857ad900889",
+    "url": "/Clicky-Game/static/js/main.912962ee.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  }
+]);
