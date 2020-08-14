bc47c59149016411e25c318ed3f697d184c66ea7
Curtis Lane <clanelmt@gmail.com>
2020-08-14 15:22:09 -0400

    correct server.js path to index.html

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname+ "/public")));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 app.listen(PORT, function() {
