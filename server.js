58f182ba20ca90868126f2f29bb05f51e3a5d4e5
Curtis Lane <clanelmt@gmail.com>
2020-08-14 15:19:51 -0400

    add server.js file for heroku deploy, and styling

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const path = require('path')
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname+ "/public")));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+app.listen(PORT, function() {
+    console.log(`App listening on port ${8080}!`);
+});
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,14 @@ import React from 'react'
 function Header({message, score, highScore, round}) {
     return(
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Avatar Clicky Game!</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light row">
+            <a className="navbar-brand col-12 col-lg-2 mt-lg-0" href="/">Avatar Clicky Game!</a>
 
-            <div className="mx-auto">
+            <div className="mx-auto col-12 col-lg-6 mt-2 mt-lg-0">
                 {message || 'Click any image to start but DON\'T click the same image twice!'}
             </div>
             
-            <div className="ml-auto" id="navbar-text">
+            <div className="ml-auto col-12 col-lg-4 mt-1 mt-lg-0" id="navbar-text">
             
                 <span className="navbar-text">
                     Score: {score} | Top Score: {highScore} | Round: {round}
