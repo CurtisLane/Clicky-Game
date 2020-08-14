71409d5ef5b662ff23dc3154971f83beadfe7bd2
Curtis Lane <clanelmt@gmail.com>
2020-08-14 15:54:30 -0400

    correct server.js path to index.html

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const express = require("express");
-const PORT = process.env.PORT || 8080;
-const path = require('path')
-
+const path = require('path');
+const express = require('express');
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname+ "/public")));
-
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+const publicPath = path.join(__dirname, '.', 'public');
+const port = process.env.PORT || 8080;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.listen(PORT, function() {
-    console.log(`App listening on port ${8080}!`);
+app.listen(port, () => {
+   console.log('Server is listening!');
 });
