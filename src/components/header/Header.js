d92a475f7708fcfee6a1fcd517bdfc33730f79de
Curtis Lane <clanelmt@gmail.com>
2020-08-17 13:23:17 -0400

    add green navbar animation when round is won

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import './header.css'
 function Header({message, score, highScore, round, gameOver, wonRound}) {
     return(
 
-        <nav className={"navbar navbar-expand-lg navbar-light row " + (gameOver === true ? 'blink-bg' : 'bg-light')}>
+        <nav className={"navbar navbar-expand-lg navbar-light row " + (gameOver === true ? 'blink-bg' : wonRound === true ? 'blink-bg-win' : 'bg-light')}>
             <a className="navbar-brand col-12 col-lg-2 mt-lg-0" href="/">Avatar Clicky Game!</a>
 
             <div className="mx-auto col-12 col-lg-6 mt-2 mt-lg-0">
